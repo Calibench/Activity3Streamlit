c1b06e57487f79a551036da5ba89740570bb9cab
Calibench <140766680+Calibench@users.noreply.github.com>
2023-07-27 21:12:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Enviroment_Temperature_change_E_All_Data_NOFLAG.csv')
+
+st.title("Enviroment Temperatures")
+st.write("[Dataset Link](https://www.kaggle.com/datasets/sevgisarac/temperature-change)")
 
