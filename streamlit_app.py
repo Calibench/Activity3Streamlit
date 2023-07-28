8d4408faf69284905b63bb3b7ab707ed1e5f34d6
Calibench <140766680+Calibench@users.noreply.github.com>
2023-07-27 21:29:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,34 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('Enviroment_Temperature_change_E_All_Data_NOFLAG.csv')
+@st.cache
+def load_data():
+    df = pd.read_csv('temperature_data.csv')
+    return df
 
-st.title('Enviroment Temperatures')
-st.write('[Dataset Link](https://www.kaggle.com/datasets/sevgisarac/temperature-change)')
+# Load the data from the CSV file
+df = load_data()
 
-selected_country = st.selectbox('Select a country', df['Area'].unique())
+# Add a title to the app
+st.title('Interactive Temperature Dashboard')
 
-filtered_data = df[(df['Area'] == selected_country)
+# Create a dropdown for area selection
+selected_area = st.selectbox('Select an Area', df['Area'].unique())
+
+# Filter the dataset based on user selections
+filtered_data = df[df['Area'] == selected_area]
+
+# Display the filtered data as a table
+st.write('Filtered Data:')
+st.write(filtered_data)
+
+# Create a bar chart using matplotlib and display it using Streamlit
+fig, ax = plt.subplots()
+years = [column for column in filtered_data.columns if 'Y' in column]
+temperatures = filtered_data[years].mean().values # assuming that we take the average temperature if an area appears multiple times
+ax.plot(years, temperatures)
+ax.set_xlabel('Year')
+ax.set_ylabel('Temperature')
+ax.set_title(f'{selected_area} - Average Temperature Over the Years')
+st.pyplot(fig)
