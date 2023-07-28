d5610337d6c68c9c290a0c3ef38dd46cc02d2346
Calibench <140766680+Calibench@users.noreply.github.com>
2023-07-27 21:45:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the data from the CSV file
-df = pd.read_csv('Environment_Temperature_change_E_All_Data_NOFLAG.csv')
+df = pd.read_csv('Environment_Temperature_change_E_All_Data_NOFLAG.csv', encoding='ISO-8859-1')
 
 # Add a title to the app
 st.title('Interactive Temperature Dashboard')
