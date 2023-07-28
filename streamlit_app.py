655ebaa7a885dc9196006afb20f59bedf994b337
Calibench <140766680+Calibench@users.noreply.github.com>
2023-07-27 21:22:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('Enviroment_Temperature_change_E_All_Data_NOFLAG.csv')
 
-st.title("Enviroment Temperatures")
-st.write("[Dataset Link](https://www.kaggle.com/datasets/sevgisarac/temperature-change)")
+st.title('Enviroment Temperatures')
+st.write('[Dataset Link](https://www.kaggle.com/datasets/sevgisarac/temperature-change)')
 
+selected_country = st.selectbox('Select a country', df['Area'].unique())
+
+filtered_data = df[(df['Area'] == selected_country)
+
+st.write('Filtered Data: ')
+st.write(filtered_data)
+
+fig, ax = plt.subplots()
